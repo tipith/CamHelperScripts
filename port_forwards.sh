b94864d03ebe8c8e8b391f382510a893b4346004
Timo Pihlström <timo.pihlstrom@iki.fi>
2016-09-21 21:52:38 +0300

    modifications

--- port_forwards.sh
+++ port_forwards.sh
@@ -7,11 +7,13 @@ if [ `hostname -s` == 'alho1' ]; then
     port='8765'
 fi
 
-
 if [ `hostname -s` == 'alho2' ]; then
     port='8766'
 fi
 
-ssh_params='-M 0 -o "ExitOnForwardFailure yes" -o "ServerAliveInterval 60" -o "ServerAliveCountMax 3" -v -f -N -R'
+if [ `hostname -s` == 'alho3' ]; then
+    port='8767'
+fi
 
-autossh $ssh_params $port:localhost:22 camserver
+autossh -M 0 -o "ExitOnForwardFailure yes" -o "ServerAliveInterval 60" -o "ServerAliveCountMax 3" -v -N -R $port:localhost:22 camserver
+
