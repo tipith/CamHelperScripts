dd4bf6951946674c2744e125fee50d39fb32ec4f
Timo Pihlström <timo.pihlstrom@iki.fi>
2016-09-21 21:36:03 +0300

    hostname based portforwards

--- port_forwards.sh
+++ port_forwards.sh
@@ -2,4 +2,16 @@
 #
 # Initiate port forwards.
 
-autossh -M 0 -o "ExitOnForwardFailure yes" -o "ServerAliveInterval 60" -o "ServerAliveCountMax 3" -v -f -N -R 8766:localhost:22 camserver
+
+if [ `hostname -s` == 'alho1' ]; then
+    port='8765'
+fi
+
+
+if [ `hostname -s` == 'alho2' ]; then
+    port='8766'
+fi
+
+ssh_params='-M 0 -o "ExitOnForwardFailure yes" -o "ServerAliveInterval 60" -o "ServerAliveCountMax 3" -v -f -N -R'
+
+autossh $ssh_params $port:localhost:22 camserver
