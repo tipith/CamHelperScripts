3875c9db95647f7901503a976e171061c5bfdba4
Timo Pihlström <timo.pihlstrom@iki.fi>
2016-09-21 22:25:17 +0300

    forgot background option

--- port_forwards.sh
+++ port_forwards.sh
@@ -15,5 +15,5 @@ if [ `hostname -s` == 'alho3' ]; then
     port='8767'
 fi
 
-autossh -M 0 -o "ExitOnForwardFailure yes" -o "ServerAliveInterval 60" -o "ServerAliveCountMax 3" -v -N -R $port:localhost:22 camserver
+autossh -M 0 -o "ExitOnForwardFailure yes" -o "ServerAliveInterval 60" -o "ServerAliveCountMax 3" -v -f -N -R $port:localhost:22 camserver
 
